e11111a6794846d4f6eb8683e7b2d14458ac09a2
boitata <ollimacgja@gmail.com>
2013-05-17 15:43:31 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gravatar2::Application.config.secret_token = '07357cd5b5881a028a4be2b0c331aa02d1b95b619540c23e653978d2aa5132d38a2f2ac397dc10a69b2939a0789a8c073320dfd72ef56d65aef7dd14bb5b36a7'
